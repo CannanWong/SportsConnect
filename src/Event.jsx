4b510b0f0c431cf7cbea4e9d1e0b12cca8fdd707
pv920 <petros.vasileiadis20@imperial.ac.uk>
2023-06-08 06:47:07 +0100

    Added EventLocationMap component to display the location of a single event.

--- src/Event.jsx
+++ src/Event.jsx
@@ -3,8 +3,9 @@ import { useLocation } from 'react-router-dom';
 import { fetchEvent } from './handles/fetchEvent';
 import { useEffect, useState } from "react";
 import { addCount } from './handles/addInterested';
+import EventLocationMap from './components/EventLocationMap';
 
-function Event() {
+function Event(props) {
   const key = useLocation().state
   const [currEvent, setCurrEvent] = useState([])
   const [count, setCount] = useState([])
@@ -13,15 +14,22 @@ function Event() {
 		fetchEvent(key.entryId, setCurrEvent, setCount)
 	}, [key.entryId])
 
+  console.log(currEvent)
 
   return (
-    <div className="container text-center">
+    <div className="container text-center justify-content-center">
       <div className="card"> 
       <div className="card-body">
         <h5 className="card-title">{currEvent.title}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">{currEvent.date} From: {currEvent.startTime} To: {currEvent.endTime}</h6>
         <p className="card-text">{currEvent.description}</p>
         <p className="interest-count">{count}</p>
+        {currEvent.location && 
+        <EventLocationMap 
+          google={props.google} 
+          location={{ lat: currEvent.location.latitude, lng: currEvent.location.longitude }} 
+        />}
+        <br/><br/>
         <button onClick={() => addCount(key.entryId, setCount)}>I'm interested!!</button>
       </div>
     </div>
---
+++ src/components/EventLocationMap.jsx
@@ -0,0 +1,25 @@
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+// Usage: <EventLocationMap google=props.google, location={{ lat: event.location.latitude, lng: event.location.longitude }} />
+function EventLocationMap(props) {
+	return (
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "end" }}>
+			<Map
+				google={props.google}
+				zoom={16}
+				initialCenter={props.location}
+				style={{ overflowX: "hidden", overflowY: "hidden" }}
+				containerStyle={{ maxWidth: "70%", maxHeight: "50%" }}
+			>
+				<Marker
+						position={props.location}
+				/>
+			</Map>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		</div>
+	);
+}
+
+export default GoogleApiWrapper({
+	apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+})(EventLocationMap);
