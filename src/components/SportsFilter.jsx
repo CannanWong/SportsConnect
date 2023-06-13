cae8d41007bf71ef6d998651b0bceac3fd25c156
pv920 <petros.vasileiadis20@imperial.ac.uk>
2023-06-13 20:53:59 +0100

    Filter by sport back-end

--- src/components/SportsFilter.jsx
+++ src/components/SportsFilter.jsx
@@ -1,14 +1,32 @@
 function SportsFilter(props) {
-	const sports = [...props.sports]
+
+	const handleCheckboxChange = (event) => {
+		const sport = event.target.id;
+		const checked = event.target.checked;
+		let temp = new Set(props.filters.sports);
+		if (checked) {
+			temp.add(sport);
+		} else {
+			temp.delete(sport);
+		}
+		props.setFilters({ ...props.filters, sports: temp })
+	}
+
   return (
 		<div class="dropdown">
   		<button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
     		Sport
   		</button>
 			<ul class="dropdown-menu">
-				{sports.map((sport) => (
+				{[...props.sports].map((sport) => (
 					<li>
-						<input class="form-check-input" type="checkbox" value="" id={sport} />
+						<input 
+							class="form-check-input" 
+							type="checkbox" 
+							id={sport} 
+							style={{ marginRight: "10px", marginLeft: "10px" }} 
+							onChange={handleCheckboxChange}
+						/>
 						<label class="form-check-label" for={sport}> {sport} </label>
 					</li>
 				))}
